bbbd7f027883582c3a7668a22455e1dd719906fd
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-03-07 10:12:46 +0200

    Connect to db and print all blogs (Ex. 3)

---
+++ cli.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const { Sequelize, Model, QueryTypes, DataTypes} = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+},
+    {
+      sequelize,
+      underscored: true,
+      timestamps: false,
+      modelName: 'note'
+    })
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    for (const blog of blogs) {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    }
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()
