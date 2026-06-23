28a9df042e7d37d61e9834159f693d1ad0dc2696
Daria Zakharova <e2402638@edu.vamk.fi>
2026-06-23 19:10:46 +0300

    Introduce users model (Ex. 8)

---
+++ controllers/users.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create({ ...req.body})
+    return res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { username: req.params.username } })
+    if (!user) {
+      return res.status(404).json({})
+    }
+
+    const { name } = req.body
+    user.name = name
+    await user.save()
+    return res.status(200).json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
