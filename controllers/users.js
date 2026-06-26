e54717c6591605e68f4dc4754e28b9fe904fb211
Daria Zakharova <e2402638@edu.vamk.fi>
2026-06-26 15:04:40 +0300

    Add user-blog associations to API responses (ex. 12)

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
 
-const { User } = require('../models')
+const { User, Blog} = require('../models')
 
 router.get('/', async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+    include: [
+      {
+        model: Blog,
+        attributes: ['author', 'url', 'title', 'likes']
+      }
+    ]
+  })
   res.json(users)
 })
 
