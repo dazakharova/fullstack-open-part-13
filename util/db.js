571c17c73ee7b245b7924a96b8c9d4941cbc1ccc
Daria Zakharova <e2402638@edu.vamk.fi>
2026-06-28 15:06:06 +0300

    Fix test environment configuration and GitHub Actions setup (ex. 17)

--- util/db.js
+++ util/db.js
@@ -1,17 +1,21 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require('sequelize')
 
 const databaseUrl = process.env.TESTING
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL
 
-const sequelize = new Sequelize(databaseUrl, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const sequelizeOptions = process.env.TESTING
+    ? {}
+    : {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
     }
-  },
-});
+
+const sequelize = new Sequelize(databaseUrl, sequelizeOptions)
 
 const connectToDatabase = async () => {
   try {
@@ -19,6 +23,7 @@ const connectToDatabase = async () => {
     console.log('connected to the database')
   } catch (err) {
     console.log('failed to connect to the database')
+    console.error(err)
     return process.exit(1)
   }
 
