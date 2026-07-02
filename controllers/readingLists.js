f359ee8e78a9e1f279df513d0837e46228da2405
Daria Zakharova <e2402638@edu.vamk.fi>
2026-07-02 21:22:50 +0300

    Add route to mark reading list as read (ex. 23)

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
-const { Blog, User, ReadingList} = require('../models')
+const { Blog, User, ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
   const { blogId, userId } = req.body
@@ -18,6 +19,30 @@ router.post('/', async (req, res, next) => {
 
     const readingList = await ReadingList.create({ blogId, userId })
     return res.status(201).json(readingList)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  const { id } = req.params
+  const { read } = req.body
+
+  try {
+    const readingList = await ReadingList.findByPk(id)
+
+    if (!readingList) {
+      return res.status(404).json({ error: 'reading list not found' })
+    }
+
+    if (readingList.userId !== req.decodedToken.id) {
+      return res.status(403).json({ error: 'not allowed to update this reading list' })
+    }
+
+    readingList.read = read
+    await readingList.save()
+
+    return res.status(200).json(readingList)
   } catch (error) {
     next(error)
   }
